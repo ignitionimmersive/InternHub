ccd291724aeabb60ec60800563de6a29432d19b5
bmtran-99 <bmtran@deakin.edu.au>
2020-06-04 23:50:14 +1000

    Minh added gaze interaction and spawning object on plane feature.

---
+++ Assets/Scripts/InfoPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPanel : MonoBehaviour
+{
+    const float speed = 6.0f;
+
+    [SerializeField] Transform infoPanel;
+
+    Vector3 scale = Vector3.zero;
+
+    void Update()
+    {
+        infoPanel.localScale = Vector3.Lerp(infoPanel.localScale, scale, Time.deltaTime * speed);
+    }
+
+    public void OpenPanel()
+    {
+        scale = Vector3.one;
+    }
+
+    public void ClosePanel()
+    {
+        scale = Vector3.zero;
+    }
+}
