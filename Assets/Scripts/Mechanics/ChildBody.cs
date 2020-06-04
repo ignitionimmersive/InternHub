3985c7de87af802f61d9f42c1eeffd1d1f25e93b
adhikarisa <66343108+adhikarisa@users.noreply.github.com>
2020-06-04 23:48:06 +1000

    Mechanics Support Touch Inputs
    Better implementation of classes, states and controllers. Works with touch input.

---
+++ Assets/Scripts/Mechanics/ChildBody.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildBody : MonoBehaviour
+{
+    public enum STATES 
+    { 
+        ASSEMBLED, 
+        DISMANTLED 
+    };
+
+    [HideInInspector]
+    public Vector3 initialPosition;
+
+    [HideInInspector]
+    public Quaternion initialRotation;
+
+
+    private STATES currentstate;
+
+    public STATES CURRENTSTATE
+    {
+        get 
+        {
+            return this.currentstate;
+        }
+        set 
+        {
+            ChangeState(value);
+        }
+    }
+
+    private void Start()
+    {
+        this.initialPosition = this.GetComponent<Transform>().position;
+        this.initialRotation = this.GetComponent<Transform>().rotation;
+        ChangeState(STATES.ASSEMBLED);
+    }
+
+    private void ChangeState(STATES _state)
+    {
+        switch (_state)
+        {
+            case STATES.ASSEMBLED:
+                {
+                    if (this.GetComponent<Rigidbody>() != null)
+                    {
+                        Destroy(this.GetComponent<Rigidbody>());
+                    }
+                    this.transform.position = this.initialPosition;
+                    this.transform.rotation = this.initialRotation;
+                    this.currentstate = _state;
+                    break;
+                }
+            case STATES.DISMANTLED:
+                {
+                    if (this.GetComponent<Rigidbody>() == null)
+                    {
+                        this.gameObject.AddComponent<Rigidbody>();
+                    }
+                    this.currentstate = _state;
+                    break;
+                }
+        }
+    }
+}
---
+++ Assets/Scripts/Mechanics/Controller.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public bool touchInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!touchInput)
+        {
+            Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100f));
+            Vector3 direction = worldMousePosition - Camera.main.transform.position;
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position, direction, out hit))
+            {
+                Debug.DrawLine(Camera.main.transform.position, worldMousePosition, Color.red);
+                if (hit.collider.gameObject.GetComponent<ChildBody>() != null)
+                {
+                    Debug.DrawLine(Camera.main.transform.position, worldMousePosition, Color.white);
+                    if (Input.GetMouseButtonUp(0))
+                    {
+                        hit.collider.gameObject.GetComponentInParent<ParentBody>().CheckState();
+                        if (hit.collider.gameObject.GetComponentInParent<ParentBody>().CURRENTSTATE == ParentBody.STATES.ASSEMBLED)
+                        {
+                            hit.collider.gameObject.GetComponentInParent<ParentBody>().DismantleAllParts();
+                        }
+                        else if (hit.collider.gameObject.GetComponentInParent<ParentBody>().CURRENTSTATE == ParentBody.STATES.DISMANTLED)
+                        {
+                            hit.collider.gameObject.GetComponentInParent<ParentBody>().AssembleIndividualParts(hit.collider.gameObject.GetComponent<ChildBody>());
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                Vector3 worldTouchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 100f));
+                Vector3 direction = worldTouchPosition - Camera.main.transform.position;
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.transform.position, direction, out hit))
+                {
+                    Debug.DrawLine(Camera.main.transform.position, worldTouchPosition, Color.red);
+                    if (hit.collider.gameObject.GetComponent<ChildBody>() != null)
+                    {
+                        Debug.DrawLine(Camera.main.transform.position, worldTouchPosition, Color.white);
+                        if (touch.phase == TouchPhase.Ended)
+                        {
+                            hit.collider.gameObject.GetComponentInParent<ParentBody>().CheckState();
+                            if (hit.collider.gameObject.GetComponentInParent<ParentBody>().CURRENTSTATE == ParentBody.STATES.ASSEMBLED)
+                            {
+                                hit.collider.gameObject.GetComponentInParent<ParentBody>().DismantleAllParts();
+                            }
+                            else if (hit.collider.gameObject.GetComponentInParent<ParentBody>().CURRENTSTATE == ParentBody.STATES.DISMANTLED)
+                            {
+                                hit.collider.gameObject.GetComponentInParent<ParentBody>().AssembleIndividualParts(hit.collider.gameObject.GetComponent<ChildBody>());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
