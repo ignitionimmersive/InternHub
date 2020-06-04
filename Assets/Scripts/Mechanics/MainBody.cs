e45cb7eec1219205ad123a08579a338b5092fab6
adhikarisa <66343108+adhikarisa@users.noreply.github.com>
2020-06-04 14:55:22 +1000

    MechanicsUpdate
    On click, every parts get dismantled, state changes to dismantled.
    clicking on every part puts it back to its original position, once every parts is reassembled, wit few seconds before game state changes.

---
+++ Assets/Scripts/Mechanics/MainBody.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+//this is the main object that contains a list individual parts
+public class MainBody : MonoBehaviour
+{
+
+    public List<SubPart> subParts;
+
+    void Start()
+    {
+        //remove rigidbodies from all objects
+        foreach (SubPart values in subParts)
+        {
+            Destroy(values.subPartElement.GetComponent<Rigidbody>());
+        }
+    }
+    public void InitialiseInitialPositionTransform(List<SubPart> _subPart)
+    {
+        foreach (SubPart values in subParts)
+        {
+            values.initialPosition = values.subPartElement.GetComponent<Transform>().position;
+            values.initialRotation = values.subPartElement.transform.rotation;
+
+        }
+    }
+
+    public void DismantleAllParts(List<SubPart> _subPart)
+    {
+
+        foreach (SubPart values in subParts)
+        {
+            DismantleIndividualParts(values);
+        }
+    }
+
+    public void DismantleIndividualParts(SubPart _part)
+    {
+        if (_part.subPartElement.GetComponent<Rigidbody>() == null)
+        {
+            _part.subPartElement.AddComponent<Rigidbody>();
+        }
+
+        _part.dismantled = true;
+
+    }
+
+    public void ReAssembleAllParts(List<SubPart> _subPart)
+    {
+        foreach (SubPart values in subParts)
+        {
+            ReAssembleIndividualSubPart(values);
+        }
+    }
+
+    public void ReAssembleIndividualSubPart(SubPart _part)
+    {
+            Destroy(_part.subPartElement.GetComponent<Rigidbody>());
+
+        _part.dismantled = false;
+        _part.subPartElement.transform.position = _part.initialPosition;
+        _part.subPartElement.transform.rotation = _part.initialRotation;
+    }
+}
+
+
+//this class is for the individual components of the main object
+[System.Serializable]
+//[RequireComponent(typeof(MainBody))]
+public class SubPart
+{
+
+    public GameObject subPartElement;
+
+    [HideInInspector]
+    public Vector3 initialPosition;
+
+    [HideInInspector]
+    public Quaternion initialRotation;
+
+    public bool dismantled
+    {
+        get; set;
+    }
+    
+}
+
+
+
+
+
---
+++ Assets/Scripts/Mechanics/MechanicsController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MainBody))]
+public class MechanicsController : MonoBehaviour
+{
+    public enum PARTSSTATE { assembled, dismantled };
+    public PARTSSTATE STATEMACHINE;
+    public MainBody theBody;
+
+    public int numberOfDismantledItems;
+
+    void Start()
+    {
+        theBody = this.GetComponent<MainBody>();
+        theBody.InitialiseInitialPositionTransform(theBody.subParts);
+        //initialise the object
+
+        InitialiseAssembly();
+        
+    }
+
+    void InitialiseAssembly()
+    {
+        theBody.ReAssembleAllParts(theBody.subParts);
+        STATEMACHINE = PARTSSTATE.assembled;
+        numberOfDismantledItems = theBody.subParts.Count;
+    }
+
+    void Update()
+    {
+        if (STATEMACHINE == PARTSSTATE.assembled)
+        {
+            InitialiseAssembly();
+
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                theBody.DismantleAllParts(theBody.subParts);
+                STATEMACHINE = PARTSSTATE.dismantled;
+            }
+        }
+
+        if (STATEMACHINE == PARTSSTATE.dismantled)
+        {
+
+            Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+            Vector3 direction = worldMousePosition - Camera.main.transform.position;
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.transform.position, direction, out hit))
+            {
+
+                if (hit.collider.gameObject.tag == "SubPart")
+                {
+                    Debug.Log(hit.transform.name);
+                    Debug.DrawLine(Camera.main.transform.position, worldMousePosition, Color.red);
+                    if (Input.GetKey(KeyCode.Mouse0))
+                    {
+                        foreach (SubPart values in theBody.subParts)
+                        {
+                            if (values.subPartElement == hit.collider.gameObject)
+                            {
+                                //if already dismantled skip
+                                if (values.dismantled == true)
+                                {
+                                    theBody.ReAssembleIndividualSubPart(values);
+
+                                    numberOfDismantledItems--;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                else
+                {
+                    Debug.DrawLine(Camera.main.transform.position, worldMousePosition, Color.white);
+                }
+            }
+            else
+            {
+                Debug.DrawLine(Camera.main.transform.position, worldMousePosition, Color.blue);
+            }
+
+
+            if (numberOfDismantledItems <= 0)
+            {
+                StartCoroutine(ChangeStateToAssembled(2f));
+            }
+        }
+    }
+
+    IEnumerator ChangeStateToAssembled(float _time)
+    {
+        //wait few seconds after everything has been dismantled
+        theBody.ReAssembleAllParts(theBody.subParts);
+        yield return new WaitForSeconds(_time);
+        STATEMACHINE = PARTSSTATE.assembled;
+    }
+}
+
+
+
