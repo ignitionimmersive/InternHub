8967f9d691408f6c18de94424b2575717580023c
adhikarisa <66343108+adhikarisa@users.noreply.github.com>
2020-06-06 00:52:38 +1000

    Mechanics for Blueprint
    Got objects to move to blueprint

--- Assets/Scripts/Mechanics/ParentBody.cs
+++ Assets/Scripts/Mechanics/ParentBody.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ParentBody : MonoBehaviour
@@ -9,6 +10,8 @@ public class ParentBody : MonoBehaviour
         ASSEMBLED, 
         DISMANTLED 
     };
+
+    public TheBlueprint theBlueprint;
 
     public List<GameObject> childParts;
 
@@ -50,92 +53,18 @@ public class ParentBody : MonoBehaviour
     {
         _parts.CURRENTSTATE = ChildBody.STATES.DISMANTLED;
     }
-}
 
-public class ChildBody : MonoBehaviour
-{
-    public enum STATES
+
+    public void MoveAllPartsToBlueprint()
     {
-        ASSEMBLED,
-        DISMANTLED
-    };
-
-    [HideInInspector]
-    public Vector3 initialPosition;
-
-    [HideInInspector]
-    public Quaternion initialRotation;
-
-
-    private STATES currentstate;
-
-    public STATES CURRENTSTATE
-    {
-        get
+        foreach (GameObject values in childParts)
         {
-            return this.currentstate;
-        }
-        set
-        {
-            ChangeState(value);
+            MoveIndividualPartToBlueprint(values.GetComponent<ChildBody>());
         }
     }
-
-    private void Start()
+    public void MoveIndividualPartToBlueprint(ChildBody _parts)
     {
-        this.initialPosition = this.GetComponent<Transform>().position;
-        this.initialRotation = this.GetComponent<Transform>().rotation;
-        ChangeState(STATES.ASSEMBLED);
-    }
-
-    private void ChangeState(STATES _state)
-    {
-        switch (_state)
-        {
-            case STATES.ASSEMBLED:
-                {
-                    if (this.GetComponent<Rigidbody>() != null)
-                    {
-                        Destroy(this.GetComponent<Rigidbody>());
-                    }
-                    this.transform.position = this.initialPosition;
-                    this.transform.rotation = this.initialRotation;
-                    this.currentstate = _state;
-                    break;
-                }
-            case STATES.DISMANTLED:
-                {
-                    if (this.GetComponent<Rigidbody>() == null)
-                    {
-                        this.gameObject.AddComponent<Rigidbody>();
-                    }
-                    this.currentstate = _state;
-                    break;
-                }
-        }
-
-        UpdateParentState();
-    }
-
-    private void UpdateParentState()
-    {
-        int i = 0;
-        foreach (GameObject values in this.gameObject.GetComponentInParent<ParentBody>().childParts)
-        {
-            if (values.GetComponent<ChildBody>().CURRENTSTATE == STATES.ASSEMBLED)
-            {
-                i++;
-            }
-        }
-
-        if (i == this.gameObject.GetComponentInParent<ParentBody>().childParts.Count)
-        {
-            this.gameObject.GetComponentInParent<ParentBody>().CURRENTSTATE = ParentBody.STATES.ASSEMBLED;
-        }
-
-        else
-        {
-            this.gameObject.GetComponentInParent<ParentBody>().CURRENTSTATE = ParentBody.STATES.DISMANTLED;
-        }
+        _parts.CURRENTSTATE = ChildBody.STATES.MOVING_TO_BLUEPRINT;
     }
 }
+
---
+++ Assets/Scripts/Mechanics/TheBlueprint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheBlueprint : MonoBehaviour
+{
+
+    //gibbous means the state of the moon between a first quarter moon and full moon
+
+    public List<GameObject> blueprintPlaceholders;
+
+
+}
