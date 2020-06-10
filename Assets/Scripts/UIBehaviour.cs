8cb59c0b064de6de74f96a2ee41a6a235a00ca93
adhikarisa <66343108+adhikarisa@users.noreply.github.com>
2020-06-10 15:07:35 +1000

    test
    test

--- Assets/Scripts/UIBehaviour.cs
+++ Assets/Scripts/UIBehaviour.cs
@@ -49,6 +49,38 @@ public class UIBehaviour : MonoBehaviour
                     StartCoroutine(disableUI());
 
                     // Mechanic.
+                    if (theParentHasBeenSpawned == false)
+                    {
+                        this.theParentHasBeenSpawned = true;
+                        theParent01 = (GameObject)GameObject.Instantiate(this.theParent.gameObject, hit.point, Quaternion.identity);
+
+                    }
+                    else if ((theBluePrintHasBeenSpawned == false) && (theParentHasBeenSpawned = true))
+                    {
+                        theBluePrint01 = (GameObject)GameObject.Instantiate(this.theBlueprint.gameObject, hit.point, Quaternion.identity);
+                        theBluePrint01.transform.Translate(0, 7, 0);
+                        theBluePrint01.transform.Rotate(-90.0f, 0.0f, 0.0f, Space.Self);
+                        theParent01.GetComponent<TheParent>().theBlueprint = theBluePrint01.GetComponent<TheBlueprint>();
+                        this.theBluePrintHasBeenSpawned = true;
+
+                    }
+
+
+                    else if ((theBluePrintHasBeenSpawned == true) && (theParentHasBeenSpawned) == true)
+                    {
+                        if (theParent01.GetComponent<TheParent>().CURRENT_STATE == TheParent.PARENT_STATE.ALL_CHILD_ON_BODY)
+                        {
+
+                            theParent01.GetComponent<TheParent>().DismantleAllChildren();
+                        }
+
+                        else if (theParent01.GetComponent<TheParent>().CURRENT_STATE == TheParent.PARENT_STATE.ALL_CHILD_ON_BLUEPRINT)
+                        {
+                            theParent01.GetComponent<TheParent>().AssembleAllChildren();
+
+                        }
+
+                    }
                 }
                 else if (open.CompareTag("UsagePanel"))
                 {
@@ -67,70 +99,6 @@ public class UIBehaviour : MonoBehaviour
                     debug.text = open.name;
                     // Place mode.
                     isPlaceModeActive = true;
-
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            //var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-            debug.text = "Not found";
-
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-                Vector3 worldTouchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 100f));
-                Vector3 direction = worldTouchPosition - Camera.main.transform.position;
-                
-                if (Physics.Raycast(ray, out RaycastHit hit))
-                {
-                    GameObject open = hit.collider.gameObject;
-
-                    if (open.CompareTag("MechanicButton"))
-                    {
-                        if (theParentHasBeenSpawned == false)
-                        {
-                                    this.theParentHasBeenSpawned = true;
-                                    theParent01 = (GameObject)GameObject.Instantiate(this.theParent.gameObject, hit.point, Quaternion.identity);
-                             
-                        }
-                        else if ((theBluePrintHasBeenSpawned == false) && (theParentHasBeenSpawned = true))
-                        {
-                                    theBluePrint01 = (GameObject)GameObject.Instantiate(this.theBlueprint.gameObject, hit.point, Quaternion.identity);
-                                    theBluePrint01.transform.Translate(0, 7, 0);
-                                    theBluePrint01.transform.Rotate(-90.0f, 0.0f, 0.0f, Space.Self);
-                                    theParent01.GetComponent<TheParent>().theBlueprint = theBluePrint01.GetComponent<TheBlueprint>();
-                                    this.theBluePrintHasBeenSpawned = true;
-                                
-                        }
-
-
-                        else if ((theBluePrintHasBeenSpawned == true) && (theParentHasBeenSpawned) == true)
-                        {
-                                if (theParent01.GetComponent<TheParent>().CURRENT_STATE == TheParent.PARENT_STATE.ALL_CHILD_ON_BODY)
-                                {
-
-                                theParent01.GetComponent<TheParent>().DismantleAllChildren();
-                                }
-
-                                else if (theParent01.GetComponent<TheParent>().CURRENT_STATE == TheParent.PARENT_STATE.ALL_CHILD_ON_BLUEPRINT)
-                                {
-                                theParent01.GetComponent<TheParent>().AssembleAllChildren();
-                                    
-                                }
-
-                        }
-                    }
-                    else if (open.CompareTag("UsageButton"))
-                    {
-                        // Usage mode.
-                    }
-                    else if (open.CompareTag("LearnButton"))
-                    {
-                        // Learn mode.
-                    }
-                    else if (open.CompareTag("PlaceButton"))
-                    {
-                        // Place mode.
-                    }
                 }
             }
         }
@@ -151,7 +119,8 @@ public class UIBehaviour : MonoBehaviour
             var asset = GameObject.FindGameObjectWithTag("Player");
             asset.SetActive(false);
         }
-        
+
         yield return new WaitForSeconds(5f);
     }
 }
+
