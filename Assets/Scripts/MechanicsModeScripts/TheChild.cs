c2ddf3b52f7852f7ee2332ba4d0c1639623e6604
DomRens <63894948+DomRens@users.noreply.github.com>
2020-07-01 10:59:26 +1000

    Sam Refactor

--- Assets/Scripts/MechanicsModeScripts/TheChild.cs
+++ Assets/Scripts/MechanicsModeScripts/TheChild.cs
@@ -26,11 +26,18 @@ public class TheChild : MonoBehaviour
 
     public CHILD_STATES CURRENTSTATE;
 
+    MoveToAPoint movePoint;
+    TheParent parent;
+    Rigidbody rigidB;
 
     private void Start()
     {
         this.initialPosition = this.gameObject.GetComponent<Transform>().position;
         this.initialRotation = this.gameObject.GetComponent<Transform>().rotation;
+        
+        movePoint = this.gameObject.GetComponent<MoveToAPoint>();
+        parent = this.gameObject.GetComponentInParent<TheParent>();
+        rigidB = this.gameObject.GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -58,7 +65,7 @@ public class TheChild : MonoBehaviour
                     this.gameObject.GetComponent<Rigidbody>().mass = 100;
                     this.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(1, 1, 0);
                     this.gameObject.GetComponent<Rigidbody>().angularDrag = 100f;
-//                    this.gameObject.GetComponent<Rigidbody>().freezeRotation = true;
+                    //this.gameObject.GetComponent<Rigidbody>().freezeRotation = true;
                     
                     // this.gameObject.GetComponent<Rigidbody>().
 
@@ -85,11 +92,13 @@ public class TheChild : MonoBehaviour
 
                     int indexInParentBody = this.gameObject.GetComponentInParent<TheParent>().children.IndexOf(this.gameObject);
 
-                    this.gameObject.GetComponent<MoveToAPoint>().finalPosition = this.gameObject.GetComponentInParent<TheParent>().theBlueprint.blueprintPlaceholders[indexInParentBody].transform.position;
-                    this.gameObject.GetComponent<MoveToAPoint>().finalRotation = this.gameObject.GetComponentInParent<TheParent>().theBlueprint.blueprintPlaceholders[indexInParentBody].transform.rotation;
+                    //this.gameObject.GetComponent<MoveToAPoint>().finalPosition = this.gameObject.GetComponentInParent<TheParent>().theBlueprint.blueprintPlaceholders[indexInParentBody].transform.position;
+                    //this.gameObject.GetComponent<MoveToAPoint>().finalRotation = this.gameObject.GetComponentInParent<TheParent>().theBlueprint.blueprintPlaceholders[indexInParentBody].transform.rotation;
+                    movePoint.finalPosition =  parent.theBlueprint.blueprintPlaceholders[indexInParentBody].transform.position;
+                    movePoint.finalRotation =  parent.theBlueprint.blueprintPlaceholders[indexInParentBody].transform.rotation;
 
-                    this.gameObject.GetComponent<MoveToAPoint>().CURRENTSTATE = MoveToAPoint.MOVE_TO_A_POINT_STATE.MOVE;
-
+                    //this.gameObject.GetComponent<MoveToAPoint>().CURRENTSTATE = MoveToAPoint.MOVE_TO_A_POINT_STATE.MOVE;
+                    movePoint.CURRENTSTATE = MoveToAPoint.MOVE_TO_A_POINT_STATE.MOVE;
 
                     this.CURRENTSTATE = CHILD_STATES.MOVING_TO_BLUEPRINT;
                     
